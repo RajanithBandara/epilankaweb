d7f8d23ae0218e393b55af0988a7119be5f9a532
RajanithBandara <RajanithBandara123@gmail.com>
2026-01-26 21:32:11 +0530

    update 404 page heading text

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,7 @@
 export default function NotFoundPage() {
       return (
         <div style={{ padding: 32 }}>
-          <h1>Page Not Found</h1>
+          <h1>Page Not Found here</h1>
           <p>The page you are looking for does not exist.</p>
         </div>
       );
